ee4ea554c3ee8cca03c48f2405a75fa695f3ee8a
bosseman <39099531+bosseman@users.noreply.github.com>
2019-12-08 17:26:33 -0600

    Update likeServlet.java

--- bookshelf-3/src/main/java/com/example/getstarted/basicactions/likeServlet.java
+++ bookshelf-3/src/main/java/com/example/getstarted/basicactions/likeServlet.java
@@ -1,56 +1,94 @@
 package com.example.getstarted.basicactions;
 
+
+
 import java.io.IOException;
+
 import java.sql.SQLException;
 
+
+
 import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
+
 import javax.servlet.http.HttpSession;
 
+
+
 import com.example.getstarted.daos.CloudSqlDao;
+
 import com.example.getstarted.daos.ProfileDao;
+
 import com.example.getstarted.objects.Profile;
 
 
+
+
+
 @WebServlet(name = "like", urlPatterns = "/like")
+
 public class likeServlet extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		
-		
-	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	     String endCursor = null;
+
+
+	    Integer endCursor;
+
 	    long likedId;
+
 		boolean matched = false;
-		endCursor = request.getParameter("cursor").toString();
-		likedId = Long.decode(request.getParameter("id"));
-		
-		ProfileDao dao = (ProfileDao) this.getServletContext().getAttribute("dao");
-		HttpSession session = request.getSession();
 		try {
+		endCursor = Integer.parseInt(request.getParameter("cursor").toString());
+		}
+		catch(NumberFormatException e) {
+			//At the end of the list
+			endCursor = -1;
+		}
+		//System.out.print("TEST CURSOR"+endCursor);
+		likedId = Long.decode(request.getParameter("id"));
+
+		
+
+		ProfileDao dao = (ProfileDao) this.getServletContext().getAttribute("dao");
+
+		HttpSession session = request.getSession();
+
+		try {
+
 			matched = dao.insertLike(Long.decode(session.getAttribute("id").toString()), likedId);
+
 		} catch (NumberFormatException | SQLException e) {
+
 			// TODO Auto-generated catch block
+
 			e.printStackTrace();
+
 		}
-		if(matched) {
-			request.setAttribute("matched", "Matched!");
+
+
+	    
+		if(endCursor >= 0) {
+			response.sendRedirect("/matching?cursor="+(endCursor-1));
 		}else {
-			request.setAttribute("matched", "No match :(");
+			//restart
+		response.sendRedirect("/matching");
 		}
-	    request.setAttribute("cursor", endCursor);
-		request.setAttribute("page","list");
-	    request.getRequestDispatcher("/base.jsp").forward(request, response);
 		
+
 		
+
 	}
+
+
 
 }
